055c20447dfab74f2abf74303df18240bcddf4ee
kobishamir <37914417+kobishamir@users.noreply.github.com>
2022-08-05 23:10:07 +0300

    Updated some of the files and created some new

--- app.py
+++ app.py
@@ -37,3 +37,43 @@ def index():
         return render_template('index.html', short_url=short_url)
 
     return render_template('index.html')
+
+
+@app.route('/<id>')
+def url_redirect(id):
+    conn = get_db_connection()
+
+    original_id = hashids.decode(id)
+    if original_id:
+        original_id = original_id[0]
+        url_data = conn.execute('SELECT original_url, clicks FROM urls'
+                                ' WHERE id = (?)', (original_id,)
+                                ).fetchone()
+        original_url = url_data['original_url']
+        clicks = url_data['clicks']
+
+        conn.execute('UPDATE urls SET clicks = ? WHERE id = ?',
+                     (clicks+1, original_id))
+
+        conn.commit()
+        conn.close()
+        return redirect(original_url)
+    else:
+        flash('Invalid URL')
+        return redirect(url_for('index'))
+
+
+@app.route('/stats')
+def stats():
+    conn = get_db_connection()
+    db_urls = conn.execute('SELECT id, created, original_url, clicks FROM urls'
+                           ).fetchall()
+    conn.close()
+
+    urls = []
+    for url in db_urls:
+        url = dict(url)
+        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        urls.append(url)
+
+    return render_template('stats.html', urls=urls)
